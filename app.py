38e0bcd1724a7554ecf4a479e2a4e17de1c8bdb0
JieamI <1732615826@qq.com>
2020-04-02 15:24:31 +0800

    V2.0

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify, request
+from flask_cors import *
+from flask_sqlalchemy import SQLAlchemy
+from spiderCourse import SpiderCourse
+import os
+import sys
+import json
+
+app = Flask(__name__)
+WIN = sys.platform.startswith('win') 
+if WIN: 
+    prefix = 'sqlite:///' 
+else:
+    prefix = 'sqlite:////'
+  
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db') 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    usr = db.Column(db.String(30), primary_key = True)
+    pwd = db.Column(db.String(30))
+    event = db.Column(db.Text)
+   
+# 课表获取API
+@app.route('/course', methods=['post'])
+@cross_origin()
+def get_tasks():
+    data = json.loads(request.get_data(as_text=True))
+    if 'usr' in data and 'pwd' in data and data['usr'] and data['pwd']:
+        spirse = SpiderCourse(data['usr'], data['pwd'])
+        try:
+            dic = {'Mon': [], 'Tue': [], 'Wed': [], 'Thu': [], 'Fri': [], 'Sat': [], 'Sun': []}
+            spirse.SoupParse()
+            for each in spirse.res:
+                index = 0
+                for value in dic.values():
+                    value.append(each[index])
+                    index += 1
+            return jsonify({'data': dic, 'status': 1})
+        except:
+            return jsonify({'status': 0})
+    else:
+        return jsonify({'status': 0})
+
+#事项云端同步更新API
+@app.route('/event', methods=['POST'])
+@cross_origin()
+def handleEvent():
+    data = json.loads(request.get_data(as_text=True))
+    if 'usr' in data and 'pwd' in data and data['usr'] and data['pwd']:
+        user = User.query.filter_by(usr = data['usr']).first()
+        if user.pwd == data['pwd']:
+            if data['method'] == 'get':
+                if user.event:
+                    eventlis = user.event.split('&')
+                    for index, val in enumerate(eventlis):
+                        temp = eventlis[index].split('|')
+                        eventlis[index] = {'content': temp[0], 'id': eval(temp[1])}
+                        if temp[2] == 'true':
+                            eventlis[index]['done'] = True
+                        elif temp[2] == 'false':
+                            eventlis[index]['done'] = False
+                    return jsonify({'eventlis':eventlis, 'status':1})
+                return jsonify({'status': 0})
+            elif data['method'] == 'post':
+                strlis = data['eventlis']['content'] + '|' + str(data['eventlis']['id']) + '|' + str(data['eventlis']['done']).lower()   
+                if user.event:
+                    user.event = user.event + '&' + strlis 
+                else:
+                    user.event = strlis
+                db.session.commit()
+                return jsonify({'status': 1})
+        return jsonify({'status': 0})
+    return jsonify({'status': 0})          
+
+#事项状态更新API
+@app.route('/switch', methods=['POST'])
+@cross_origin()
+def handleSwitch():
+    data = json.loads(request.get_data(as_text=True))
+    if 'usr' in data and 'pwd' in data and data['usr'] and data['pwd']:
+        user = User.query.filter_by(usr = data['usr']).first()
+        if user.pwd == data['pwd']:
+            if data['eventlis']['done']:
+                user.event = user.event.replace(str(data['eventlis']['id']) + '|' + 'false', str(data['eventlis']['id']) + '|' + "true")
+            else:
+                user.event = user.event.replace(str(data['eventlis']['id']) + '|' + 'true', str(data['eventlis']['id']) + '|' + "false")
+            db.session.commit()
+            print(user.event)
+            return jsonify({'status': 1})
+        return jsonify({'status': 0})
+    return jsonify({'status': 0})
---
+++ spiderCourse.py
@@ -0,0 +1,40 @@
+import requests
+import execjs
+from bs4 import BeautifulSoup
+
+class SpiderCourse():
+    # 初始化
+    def __init__(self, usr, pwd):
+        self.url = 'http://202.114.50.129/Certification/login.do'
+        self.datas = {'userName1': execjs.compile(open(r"md5.js").read()).call('hex_md5', usr),
+                      'password1': execjs.compile(open(r"sha1.js").read()).call('hex_sha1', usr + pwd),
+                      'webfinger': 'ec198773ef514e44ed8cbf9ef7a114e7',
+                      'type': 'xs',     #身份认证为学生
+                      'userName': usr,
+                      'password': pwd
+                    } 
+                                      
+        self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+    # 网页解析
+    def SoupParse(self):
+        # 由浏览器指纹获取code
+        fp = requests.post(url = "http://sso.jwc.whut.edu.cn/Certification/getCode.do", data = {'webfinger': self.datas['webfinger']})
+        self.datas['code'] = fp.content.decode('utf-8') 
+        html = requests.post(self.url, data = self.datas,headers = self.header)
+        soup = BeautifulSoup(html.content, "lxml")
+        course = soup.select_one(".table-class-even")
+        self.res=[[],[],[],[],[]]
+        index = 0
+        for each in course.find_all('tr',recursive=False):
+            for child in each.find_all('td',{'style':'text-align: center'},recursive=False,limit=7):
+                temp_blue = child.find('div',{'style':'margin-top: 2px; font-size: 10px; color: blue'})
+                temp_red = child.find('div',{'style':'margin-top: 2px; font-size: 10px; color: red'}) 
+                if temp_blue or temp_red:
+                    if temp_blue:
+                        self.res[index].append(temp_blue.get_text().strip().replace('\t','').replace('\r', '').replace('\n', ''))
+                    else:
+                        self.res[index].append(temp_red.get_text().strip().replace('\t','').replace('\r', '').replace('\n', ''))
+                else:
+                    self.res[index].append("")
+            index = index+1       
+
